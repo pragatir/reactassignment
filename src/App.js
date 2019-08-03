d337db50da63cad59aa13813fff59dae38cbb70a
Ritesh Firodiya <firodiya.ritesh@confluxsys.com>
2019-08-03 16:12:20 +0530

    mock data added for inital route

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import "./App.css";
@@ -10,26 +10,39 @@ import History from "./components/History";
 const formTitle = "Enquiry Form";
 
 const App = () => {
-  const [feedbacks, setFeedbacks] = useState([{
-    "fullName": "Ritesh Firodiya",
-    "email": "firodiya.ritesh@gmail.com",
-    "phoneNo": "9226737797",
-    "message": "Hi Globant!",
-    "country": "India",
-    "gender": "Male",
-  }]);
+  const [feedbacks, setFeedbacks] = useState([
+    {
+      "fullName": "Ritesh Firodiya",
+      "email": "firodiya.ritesh@gmail.com",
+      "phoneNo": "9226737797",
+      "message": "Hi Globant!, this is sample initial mock data 1",
+      "country": "India",
+      "gender": "Male",
+    },
+    {
+      "fullName": "Ritesh Firodiya",
+      "email": "firodiya.ritesh@gmail.com",
+      "phoneNo": "9226737797",
+      "message": "Hi Globant!, this is sample initial mock data 2",
+      "country": "India",
+      "gender": "Male",
+    }
+  ]);
   const submitFeedback = (feedback) => setFeedbacks([...feedbacks, feedback]);
-  
+
+  useEffect(() => {
+    console.log(feedbacks);
+  }, [feedbacks]);
   return (
     <div className="App">
       <NavBar title="Enquiry Site" />
       <div className="App-header">
         <Router>
           <Route path="/" exact
-            render={() => <FormWrapper submitFeedback={submitFeedback} formTitle={formTitle}/>}
+            render={(props) => <FormWrapper submitFeedback={submitFeedback} formTitle={formTitle} />}
           />
           <Route path="/history" exact
-            render={() => <History feedbacks={feedbacks} />}
+            render={(props) => <History feedbacks={feedbacks} />}
           />
         </Router>
       </div>
--- src/components/History.js
+++ src/components/History.js
@@ -1,18 +1,32 @@
 import React from "react";
-import { Card } from "react-bootstrap";
+import { Card, Row, Col } from "react-bootstrap";
+
+import FormField from "./FormField";
 
 const Histroy = ({ feedbacks } = {}) => {
     return (
-        <>
+        <div className="container">
+            <FormField formTitle={"History"} style={{ textAlign: "center" }} />
             {feedbacks.map((feedback, index) => (
-                <Card className="" key={index}>
-                    <Card.Title>{feedback.fullName}</Card.Title>
-                    <Card.Body>
-                        {feedback.email}
-                    </Card.Body>
-                </Card>
+                <div key={index}>
+                    <Card style={{ color: "black" }}>
+                        <Card.Header>{feedback.fullName}</Card.Header>
+                        <Card.Body>
+                            <Row>
+                                <Col><b>Email:</b>{feedback.email}</Col>
+                                <Col><b>Phone No: </b>{feedback.phoneNo}</Col>
+                                <Col><b>Gender:</b>{feedback.gender}</Col>
+                                <Col><b>Country:</b>{feedback.country}</Col>
+                            </Row>
+                            <Row>
+                                <Col><b>Message: </b>{feedback.message}</Col>
+                            </Row>
+                        </Card.Body>
+                    </Card>
+                    <br />
+                </div>
             ))}
-        </>
+        </div>
     )
 }
 
